1da296df75bf5433bca5f3febd248b2a14645a18
saiTharunDusa <sd2672@nau.edu>
2024-05-15 21:38:10 -0700

    "created own create-react-app without using the command "npx create-react-app"

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const heading = React.createElement("h1", {id: "header" }, "Hello Sai Tharun Dusa!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
