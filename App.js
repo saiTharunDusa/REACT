f5fda1fe5de428ca58a6531f8ec2ec150bf93e0b
saiTharunDusa <sd2672@nau.edu>
2024-05-15 19:25:46 -0700

    react - 01

--- App.js
+++ App.js
@@ -4,3 +4,4 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const parent = React.createElement("div", {id : "parent"}, React.createElement("div", {id : "child"}, [React.createElement("h1", {}, "I'm a H1 tag"), React.createElement("h2", {}, "I'm a H2 tag")]));
 
 root.render(parent);
+
