757a42440d9e3777862419f168e5b98b07da2437
saiTharunDusa <sd2672@nau.edu>
2024-05-17 16:52:44 -0700

    Learnt JSX, components and component composition

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { jsx } from "react/jsx-runtime";
 
 
-const heading = React.createElement("h1", {id: "header" }, "Hello Sai Tharun Dusa!");
+// writing in JSX
+const jsx_heading = <h1 id="heading">hello world using jsx</h1>;
+
+// react functional component.
+const HeadingComponent = () => {
+    return (
+        <div id = "container">
+            <h1 id ="heading"> heading from react functional component</h1>
+        </div>
+        
+    );
+}
+
+// component composition
+const HeadingComponent1 = ()=>{
+    return (
+        <div> 
+            <HeadingComponent/>
+            <h1>heading after component composistion.</h1>
+        </div>
+    )
+}
+
+// In jsx, insider {} braces we could write js expressions
+const number = 10;
+const HeadingComponent2 = ()=>{
+    return (
+        <div> 
+            <HeadingComponent/>
+            <HeadingComponent1/>
+            <h1>heading after component composistion.</h1>
+            <h2>using {} braces for writing js expressions inside jsx</h2>
+            <h2>{number}</h2>
+        </div>
+    )
+}
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const parent = React.createElement("div", {id : "parent"}, React.createElement("div", {id : "child"}, [React.createElement("h1", {}, "I'm a H1 tag"), React.createElement("h2", {}, "I'm a H2 tag")]));
-
-root.render(parent);
+root.render(<HeadingComponent2/>);
 
