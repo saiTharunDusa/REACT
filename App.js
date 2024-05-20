a309a1e4cc501dd93d3a13a7bcaa7f6c8a88f6b8
saiTharunDusa <sd2672@nau.edu>
2024-05-20 15:31:22 -0700

    assignment3 - Implemented a sample header component

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { jsx } from "react/jsx-runtime";
-
+import {HeadingComponent} from "./HeadingComponent";
 
 // writing in JSX
 const jsx_heading = <h1 id="heading">hello world using jsx</h1>;
 
-// react functional component.
-const HeadingComponent = () => {
-    return (
-        <div id = "container">
-            <h1 id ="heading"> heading from react functional component</h1>
-        </div>
+// // react functional component.
+// const HeadingComponent = () => {
+//     return (
+//         <div id = "container">
+//             <h1 id ="heading"> heading from react functional component</h1>
+//         </div>
         
-    );
-}
+//     );
+// }
 
 // component composition
 const HeadingComponent1 = ()=>{
@@ -41,10 +41,21 @@ const HeadingComponent2 = ()=>{
 }
 
 
+/*
+    Assignment3
+*/
+const header_element = React.createElement("div", {className:"title"},[ React.createElement("h1", {} , "hello world"), React.createElement("h2", {}, "hello world"), React.createElement("h3", {}, "hello world")] );
+
+// using JSX
+const header_element_JSX = <div className="title">
+    <h1>hello world1</h1>
+    <h2>hello world2</h2>
+    <h3>hello world3</h3>
+</div>
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent2/>);
+root.render(<HeadingComponent/>);
 
