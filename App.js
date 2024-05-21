9917f344bdf436882a8d8ba8758142ff887b3072
saiTharunDusa <sd2672@nau.edu>
2024-05-20 23:11:48 -0700

    Implemented header and body components. And added some cards in the body by passing data as props

--- App.js
+++ App.js
@@ -1,61 +1,86 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { jsx } from "react/jsx-runtime";
-import {HeadingComponent} from "./HeadingComponent";
-
-// writing in JSX
-const jsx_heading = <h1 id="heading">hello world using jsx</h1>;
-
-// // react functional component.
-// const HeadingComponent = () => {
-//     return (
-//         <div id = "container">
-//             <h1 id ="heading"> heading from react functional component</h1>
-//         </div>
-        
-//     );
-// }
-
-// component composition
-const HeadingComponent1 = ()=>{
-    return (
-        <div> 
-            <HeadingComponent/>
-            <h1>heading after component composistion.</h1>
-        </div>
-    )
-}
-
-// In jsx, insider {} braces we could write js expressions
-const number = 10;
-const HeadingComponent2 = ()=>{
-    return (
-        <div> 
-            <HeadingComponent/>
-            <HeadingComponent1/>
-            <h1>heading after component composistion.</h1>
-            <h2>using {} braces for writing js expressions inside jsx</h2>
-            <h2>{number}</h2>
-        </div>
-    )
-}
-
+import logo from "./logo.png";
+import cart from "./cart.png";
 
 /*
-    Assignment3
+    Building a food delivery app.
+    1. Types of components that could be included in the app.
+    -Header:
+        - Logo
+        - Nav items
+    -Body:
+        - Search Bar
+        - Restaurant Container
+            - Restaurant Card
+                - image
+                - Name of the Res, rating, delivery time
+    -Footer:
+        -Copyright
+        -Links
+        -Address
+        -Contact
 */
-const header_element = React.createElement("div", {className:"title"},[ React.createElement("h1", {} , "hello world"), React.createElement("h2", {}, "hello world"), React.createElement("h3", {}, "hello world")] );
 
-// using JSX
-const header_element_JSX = <div className="title">
-    <h1>hello world1</h1>
-    <h2>hello world2</h2>
-    <h3>hello world3</h3>
-</div>
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+            <img className = "logo" src = {logo}></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li><img className="cart" src = {cart}></img></li>
+                </ul>
+            </div>
+            
+        </div>
+    );
+}
 
+const RestaurantCard = ({resName, cuisine}) => {
+    return (
+        <div className="res-card">
+            
+            <img className = "res-img"src = "https://b.zmtcdn.com/data/pictures/9/20983729/85d9095f435204f8b3638c47b9ac7fb0_featured_v2.jpg"/>
+            <h3>{resName}</h3>
+            <h4>{cuisine}</h4>
+            <h4>4.4 Stars</h4>
+            <h4>38 Del Time</h4>
+        </div>
+    );
+}
 
+const Body = () => {
+    return(
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="res-container">
+                <RestaurantCard resName = "Meghana Food" cuisine = "Biryani, North Indian, Asian"/>
+                <RestaurantCard resName = "KFC" cuisine = "Chicken, Fast Food"/>
+                
+            </div>
+
+        </div>
+    );
+}
+
+const App = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<App/>);
 
