168b3dd784c463181b0a44244f5848b1e97d824f
saiTharunDusa <sd2672@nau.edu>
2024-05-24 21:51:00 -0700

    implemented a feature for a 'top-rated' button, which filters restaurants with ratings above 4. Additionally, I implemented a reset button using the React hook useState().

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
 import resObj from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+    const [resList, setResData] = useState(resObj);
+    const [originalList, setOriginalList] = useState(resObj);
     return(
         <div className="body">
-            <div className="search">
-                Search
+            <div className="filter-btn">
+                <button onClick={()=>{
+                    const filteredResList = resList.filter((res) => res.info.avgRating > 4);
+                    setResData(filteredResList);
+                }}> Top Rated Restaurants </button>
+                <button onClick={()=>{
+                    setResData(originalList);
+                }}>Reset</button>
             </div>
             <div className="res-container">
                 {
-                    resObj.map((resData) => {
+                    resList.map((resData) => {
                         return(
                             <RestaurantCard key = {resData.info.id} resData = {resData}/>
                         )
